43cb9ae1e0fc37e9f2c30faf191893273e8605a7
Marek Grabarz <m.grabarz@wp.pl>
2018-12-28 19:35:11 +0100

    Image rebased to debian.

--- cloudflared/alpine/entrypoint/entrypoint.sh
+++ cloudflared/alpine/entrypoint/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/sh
 
 echo ${BASE64_ACCOUNT_PEM} | base64 -d > /etc/cloudflared/cert.pem
-exec /etc/cloudflared/cloudflared tunnel --hostname ${DEST_URL} --hello-world
+exec /usr/sbin/cloudflared/cloudflared tunnel --hostname ${DEST_URL} ${SRC_URL} "$@"
